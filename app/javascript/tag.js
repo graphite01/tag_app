46951a94c8649b4df191a6984f701a5974eeaf49
graphite01 <kirito10082279@gmail.com>
2022-11-25 17:16:11 +0900

    tag.js、keywordの中身確認

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputElement = document.getElementById("post_form_tag_name");
     inputElement.addEventListener("input", () => {
       const keyword = document.getElementById("post_form_tag_name").value;
+      console.log(keyword);
     });
   };
 });
